fff2f2a7746f43f1a2102b03e640751086e80bf0
정승우 <tkdltlf@naver.com>
2023-06-03 20:52:48 +0900

    next

--- Account /src/main/java/com/example/springboot/Account/entity/Accounts.java
+++ Account /src/main/java/com/example/springboot/Account/entity/Accounts.java
@@ -1,18 +1,23 @@
 package com.example.springboot.Account.entity;
 
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Getter
 @Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Accounts {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     String account_id;
 
     String account_pwd;
